2eabfe5bb58dcf0a5dae861de24eb3decdcf9a0f
QuiD-0 <wodnd101@naver.com>
2023-08-03 16:08:49 +0900

    비즈니스 로직 생성

--- batch/src/main/java/com/quid/batch/coupon/usecase/AddFromExcelCoupon.kt
+++ batch/src/main/java/com/quid/batch/coupon/usecase/AddFromExcelCoupon.kt
@@ -1,10 +1,12 @@
 package com.quid.batch.coupon.usecase
 
+import com.quid.batch.coupon.domain.Coupon
 import com.quid.batch.coupon.repository.CouponRepository
 import org.springframework.batch.core.Step
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
 import org.springframework.batch.repeat.RepeatStatus
 import org.springframework.stereotype.Service
+import java.io.File
 
 interface AddFromExcelCoupon {
     fun execute(): Step
@@ -18,11 +20,37 @@ interface AddFromExcelCoupon {
         override fun execute(): Step =
             stepBuilderFactory.get("addFromExcelCoupon")
                 .tasklet { _, _ ->
-                    readCouponListFromExcel()
-                        .also{ couponRepository.saveAll(it) }
+                    takeIf { checkExcelFile() }
+                         ?.let{
+                             readCouponListFromExcel()
+                                 .also{ couponRepository.saveAll(it) }
+                                 .also { completeExcelFile() }
+                         }
+                        ?: throw IllegalArgumentException("Excel file already published")
                     RepeatStatus.FINISHED
                 }
                 .build()
+
+        private fun completeExcelFile() {
+            val excelFile = File(EXCEL_FILE_NAME)
+            excelFile.renameTo(File("${excelFile.absolutePath}.complete"))
+        }
+
+        private fun checkExcelFile(): Boolean {
+            val excelFile = File(EXCEL_FILE_NAME)
+            return excelFile.exists()
+        }
+
+        private fun readCouponListFromExcel(): List<Coupon> {
+            val couponList = mutableListOf<Coupon>()
+            val excelFile = File(EXCEL_FILE_NAME)
+            println(excelFile.absolutePath)
+            return couponList
+        }
+
+        companion object {
+            private const val EXCEL_FILE_NAME = "upload/addCouponList.xlsx"
+        }
 
     }
 }
--- batch/src/main/java/com/quid/batch/job/JobConfig.kt
+++ batch/src/main/java/com/quid/batch/job/JobConfig.kt
@@ -19,13 +19,13 @@ class JobConfig(
 ) {
     @Bean("couponJob")
     fun couponJob(): Job = jobBuilderFactory.get("couponJob")
-        .start(publishCoupon.execute())
-        .next(deleteExpiredCoupon.execute())
-        .next(addFromExcelCoupon.execute())
+//        .start(publishCoupon.execute())
+//        .next(deleteExpiredCoupon.execute())
+        .start(addFromExcelCoupon.execute())
         .build()
 
-    @Bean("downloadJob")
-    fun downloadJob(): Job = jobBuilderFactory.get("downloadJob")
-        .start(couponListExcel.download())
-        .build()
+//    @Bean("downloadJob")
+//    fun downloadJob(): Job = jobBuilderFactory.get("downloadJob")
+//        .start(couponListExcel.download())
+//        .build()
 }
