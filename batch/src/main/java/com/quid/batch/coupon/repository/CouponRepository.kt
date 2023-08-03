9ac76766de277b060dea72d3bbfb71afaecbdfdb
QuiD-0 <wodnd101@naver.com>
2023-08-03 15:46:52 +0900

    save all

--- batch/src/main/java/com/quid/batch/coupon/repository/CouponRepository.kt
+++ batch/src/main/java/com/quid/batch/coupon/repository/CouponRepository.kt
@@ -1,12 +1,14 @@
 package com.quid.batch.coupon.repository
 
 import com.quid.batch.coupon.domain.Coupon
+import com.quid.batch.coupon.domain.createCoupon
 import com.quid.batch.coupon.repository.jpa.CouponJpaRepository
 import org.springframework.stereotype.Repository
 import java.time.LocalDateTime
 
 interface CouponRepository {
     fun findUsableCoupon(): List<Coupon>
+    fun saveAll(couponList: List<Coupon>): List<Coupon>
 
     @Repository
     class CouponRepositoryImpl(
@@ -20,5 +22,10 @@ interface CouponRepository {
             )
                 .map { it.toDomain() }
         }
+
+        override fun saveAll(couponList: List<Coupon>): List<Coupon> =
+            couponList.map { toEntity{ it } }
+                .let { couponJpaRepository.saveAll(it) }
+                .map { it.toDomain() }
     }
 }
